6c1e10185c35123e3de4c454d8675c5c50ca3f2f
ohbyeongmin <0x2biimi@gmail.com>
2025-11-17 22:45:40 +0900

    feat: Manage participants

--- src/LiberiaEvent.sol
+++ src/LiberiaEvent.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract LiberiaEvent is AccessControl {
+    bytes32 public constant SYSTEM_ADMIN_ROLE = keccak256("SYSTEM_ADMIN_ROLE");
     bytes32 public constant APPROVER_ROLE = keccak256("APPROVER_ROLE");
     bytes32 public constant VERIFIER_ROLE = keccak256("VERIFIER_ROLE");
 
@@ -13,6 +14,12 @@ contract LiberiaEvent is AccessControl {
     uint256 public amountPerDay;
     uint256 public maxParticipants;
     uint256 public maxTotalPayments;
+
+    mapping(address => bool) public isParticipant;
+    uint256 public participantCount;
+
+    event ParticipantRegistered(address indexed participant, address indexed admin);
+    event ParticipantRemoved(address indexed participant, address indexed admin);
 
     constructor(
         address _usdcAddress,
@@ -33,6 +40,7 @@ contract LiberiaEvent is AccessControl {
         maxTotalPayments = _maxTotalPayments;
 
         _grantRole(DEFAULT_ADMIN_ROLE, _systemAdmin);
+        _grantRole(SYSTEM_ADMIN_ROLE, _systemAdmin);
 
         for (uint256 i = 0; i < _approvers.length; i++) {
             _grantRole(APPROVER_ROLE, _approvers[i]);
@@ -40,5 +48,30 @@ contract LiberiaEvent is AccessControl {
         for (uint256 i = 0; i < _verifiers.length; i++) {
             _grantRole(VERIFIER_ROLE, _verifiers[i]);
         }
+    }
+
+    function registerParticipant(address participant) external onlyRole(SYSTEM_ADMIN_ROLE) {
+        _registerParticipant(participant);
+    }
+
+    function registerParticipants(address[] memory participants) external onlyRole(SYSTEM_ADMIN_ROLE) {
+        for (uint256 i = 0; i < participants.length; i++) {
+            _registerParticipant(participants[i]);
+        }
+    }
+
+    function _registerParticipant(address participant) private {
+        require(participant != address(0), "Invalid participant address");
+        require(participantCount < maxParticipants, "Max participants reached");
+        isParticipant[participant] = true;
+        participantCount++;
+        emit ParticipantRegistered(participant, msg.sender);
+    }
+
+    function removeParticipant(address participant) external onlyRole(SYSTEM_ADMIN_ROLE) {
+        require(isParticipant[participant], "Participant not registered");
+        isParticipant[participant] = false;
+        participantCount--;
+        emit ParticipantRemoved(participant, msg.sender);
     }
 }
--- test/LiberiaEvent.t.sol
+++ test/LiberiaEvent.t.sol
@@ -9,6 +9,9 @@ contract LiberiaEventTest is Test {
     address public usdcAddress;
     address public systemAdmin;
 
+    event ParticipantRegistered(address indexed participant, address indexed admin);
+    event ParticipantRemoved(address indexed participant, address indexed admin);
+
     function setUp() public {
         usdcAddress = address(0x1234);
         systemAdmin = address(this);
@@ -82,5 +85,137 @@ contract LiberiaEventTest is Test {
         bytes32 defaultAdminRole = eventContract.DEFAULT_ADMIN_ROLE();
 
         assertTrue(eventContract.hasRole(defaultAdminRole, systemAdmin));
+    }
+
+    function test_AllowSystemAdminToRegisterAParticipant() public {
+        address participant = address(0x9999);
+
+        vm.prank(systemAdmin);
+        eventContract.registerParticipant(participant);
+
+        assertTrue(eventContract.isParticipant(participant));
+    }
+
+    function test_EmitParticipantRegisteredEventWhenParticipantIsRegistered() public {
+        address participant = address(0x9999);
+
+        vm.expectEmit(true, true, false, false, address(eventContract));
+        emit ParticipantRegistered(participant, systemAdmin);
+
+        vm.prank(systemAdmin);
+        eventContract.registerParticipant(participant);
+    }
+
+    function test_RevertWhenNonSystemAdminTriesToRegisterParticipant() public {
+        address participant = address(0x9999);
+        address nonAdmin = address(0x8888);
+
+        vm.prank(nonAdmin);
+        vm.expectRevert();
+        eventContract.registerParticipant(participant);
+    }
+
+    function test_RevertWhenRegisteringZeroAddressAsParticipant() public {
+        vm.prank(systemAdmin);
+        vm.expectRevert("Invalid participant address");
+        eventContract.registerParticipant(address(0));
+    }
+
+    function test_RevertWhenParticipantCountExceedsMaxParticipants() public {
+        // maxParticipants is set to 50 in setUp
+        // Register 50 participants
+        vm.startPrank(systemAdmin);
+        for (uint256 i = 1; i <= 50; i++) {
+            eventContract.registerParticipant(address(uint160(i)));
+        }
+
+        // Try to register the 51st participant, should revert
+        vm.expectRevert("Max participants reached");
+        eventContract.registerParticipant(address(uint160(51)));
+        vm.stopPrank();
+    }
+
+    function test_AllowRegisteringMultipleParticipantsAtOnce() public {
+        address[] memory participants = new address[](3);
+        participants[0] = address(0x1001);
+        participants[1] = address(0x1002);
+        participants[2] = address(0x1003);
+
+        vm.prank(systemAdmin);
+        eventContract.registerParticipants(participants);
+
+        assertTrue(eventContract.isParticipant(participants[0]));
+        assertTrue(eventContract.isParticipant(participants[1]));
+        assertTrue(eventContract.isParticipant(participants[2]));
+        assertEq(eventContract.participantCount(), 3);
+    }
+
+    function test_AllowSystemAdminToRemoveAParticipant() public {
+        address participant = address(0x9999);
+
+        vm.startPrank(systemAdmin);
+        eventContract.registerParticipant(participant);
+        assertTrue(eventContract.isParticipant(participant));
+        assertEq(eventContract.participantCount(), 1);
+
+        eventContract.removeParticipant(participant);
+        assertFalse(eventContract.isParticipant(participant));
+        assertEq(eventContract.participantCount(), 0);
+        vm.stopPrank();
+    }
+
+    function test_EmitParticipantRemovedEventWhenParticipantIsRemoved() public {
+        address participant = address(0x9999);
+
+        vm.startPrank(systemAdmin);
+        eventContract.registerParticipant(participant);
+
+        vm.expectEmit(true, true, false, false, address(eventContract));
+        emit ParticipantRemoved(participant, systemAdmin);
+
+        eventContract.removeParticipant(participant);
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenRemovingNonExistentParticipant() public {
+        address participant = address(0x9999);
+
+        vm.prank(systemAdmin);
+        vm.expectRevert("Participant not registered");
+        eventContract.removeParticipant(participant);
+    }
+
+    function test_TrackTotalParticipantCountCorrectly() public {
+        vm.startPrank(systemAdmin);
+
+        // Initially count should be 0
+        assertEq(eventContract.participantCount(), 0);
+
+        // Register 3 participants
+        eventContract.registerParticipant(address(0x1001));
+        assertEq(eventContract.participantCount(), 1);
+
+        eventContract.registerParticipant(address(0x1002));
+        assertEq(eventContract.participantCount(), 2);
+
+        eventContract.registerParticipant(address(0x1003));
+        assertEq(eventContract.participantCount(), 3);
+
+        // Remove 1 participant
+        eventContract.removeParticipant(address(0x1002));
+        assertEq(eventContract.participantCount(), 2);
+
+        // Register another participant
+        eventContract.registerParticipant(address(0x1004));
+        assertEq(eventContract.participantCount(), 3);
+
+        // Remove 2 participants
+        eventContract.removeParticipant(address(0x1001));
+        assertEq(eventContract.participantCount(), 2);
+
+        eventContract.removeParticipant(address(0x1003));
+        assertEq(eventContract.participantCount(), 1);
+
+        vm.stopPrank();
     }
 }
